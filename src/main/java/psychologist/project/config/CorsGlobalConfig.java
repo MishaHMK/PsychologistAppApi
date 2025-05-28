20490bc13b3658bacb6c97f9fc5c237920731394
MishaHMK <gumeniukmich@gmail.com>
2025-05-28 18:22:15 +0300

    cors fix

--- src/main/java/psychologist/project/config/CorsGlobalConfig.java
+++ src/main/java/psychologist/project/config/CorsGlobalConfig.java
@@ -13,7 +13,7 @@ public class CorsGlobalConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173, http://localhost:3000",
-                "https://taniavozniuk.github.io/*"));
+                "https://taniavozniuk.github.io"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
